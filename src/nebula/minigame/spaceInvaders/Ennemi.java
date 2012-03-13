fd0bf646a03de2bd38eed39699eaa9ffa1136a95
maugard.matthieu <maugard.matthieu@gmail.com>
2012-03-13 13:58:01 +0000

    V2 de Space Invaders!! Bruitage, collision améliorée, le bonheur !

--- src/nebula/minigame/spaceInvaders/Ennemi.java
+++ src/nebula/minigame/spaceInvaders/Ennemi.java
@@ -6,14 +6,19 @@ import org.newdawn.slick.*;
 
 public class Ennemi 
 {
-	private Image image = null;
+	private static Image image = null;
 	private float x = 0;
 	private float y = 0;
 	private int pts = 0;
+	private static Sound son = null;
 	
 	public Ennemi(float x, float y) throws SlickException
 	{
-		image = new Image("assets/spaceInvaders/ship.png");
+		if(image == null)
+			image = new Image("assets/images/spaceInvaders/ship.png");
+		if(son == null)
+			son = new Sound("assets/sound/spaceInvaders/explosion.ogg");
+		
 		this.x = x;
 		this.y = y;
 		this.pts = 100;
@@ -21,16 +26,14 @@ public class Ennemi
 	
 	public boolean touche(Tir tir)
 	{
-		return Collision.rectangle(this.getX(),this.getY(), this.getImage().getWidth(), this.getImage().getHeight(), tir.getX(), tir.getY(), tir.getImage().getWidth(), tir.getImage().getHeight()/3);
-		/*if(tir.getX() + tir.getImage().getWidth()/2 > this.getX()
-				&& tir.getX() + tir.getImage().getWidth()/2 < this.getX() + this.getImage().getWidth()
-    			&& tir.getY() <= this.getY() + this.getImage().getHeight() 
-    			&& tir.getY() >= this.getY())
-			return true;
-		else
-			return false;*/
+		return Collision.rectangle(this.getX(),this.getY(), 3*this.getImage().getWidth()/4, 3*this.getImage().getHeight()/4, tir.getX(), tir.getY(), tir.getImage().getWidth(), tir.getImage().getHeight()/3);
 	}
-
+	
+	public Sound getSon()
+	{
+		return son;
+	}
+	
 	public Image getImage() {
 		return image;
 	}
