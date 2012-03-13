d01400dfb91d32929d0765445519881d97cf546a
thomas.dimeco <thomas.dimeco@gmail.com>
2012-03-13 14:45:31 +0000

    Added collision breakout

---
+++ src/nebula/minigame/breakout/Ball.java
@@ -0,0 +1,69 @@
+package nebula.minigame.breakout;
+
+
+import nebula.core.helper.Collision;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+
+/**
+ * Ball class
+ */
+public class Ball
+{
+    public static final float w = 48;
+    public static final float h = 48;
+    
+    private float x;
+    private float y;
+    private static Image image;
+
+    public Ball () throws SlickException
+    {
+        // Set ball image first time
+        if (image == null)
+            image = new Image(Breakout.rccPath + "ball.png");
+    }
+    
+    public boolean collideBrick (Brick b)
+    {
+        return Collision.rectangle(
+            x, y, w, h,
+            b.getX(), b.getY(), b.getWidth(), b.getHeight()
+        );
+    }
+    
+    public boolean collideRacket (Racket r)
+    {
+        return Collision.rectangle(
+            x, y, w, h,
+            r.getX(), r.getY(), Racket.w, Racket.h
+        );
+    }
+
+    public void draw ()
+    {
+        image.draw(x, y, w, h);
+    }
+    
+    public void setX (float x)
+    {
+        this.x = x;
+    }
+    
+    public float getX ()
+    {
+        return x;
+    }
+    
+    public void setY (float y)
+    {
+        this.y = y;
+    }
+    
+    public float getY ()
+    {
+        return y;
+    }
+}
--- src/nebula/minigame/breakout/Breakout.java
+++ src/nebula/minigame/breakout/Breakout.java
@@ -3,6 +3,8 @@ package nebula.minigame.breakout;
 import java.util.ArrayList;
 import java.util.List;
 
+import nebula.core.helper.Collision;
+
 import org.newdawn.slick.*;
 
 
@@ -16,6 +18,8 @@ public class Breakout extends BasicGame
     
     private float gameActiveCounter;
     private float whiteFadeAlpha;
+    private float[] ballSpeed = new float[2];
+    private Ball ball;
     private Racket racket;
     private List<Brick> bricks = new ArrayList<Brick>();
     private Image bgImage;
@@ -38,9 +42,14 @@ public class Breakout extends BasicGame
         gameActiveCounter = 1.0f;
         whiteFadeAlpha = 0.0f;
         
+        // Ball and racket
+        ball = new Ball();
         racket = new Racket(0, gc.getWidth(),
             gc.getHeight()+Racket.h, gc.getHeight()-Racket.h - 30);
         
+        racket.attachBall(ball);
+        
+        // Bricks
         BricksField field
             = new BricksField(0, 0, gc.getWidth(), gc.getHeight()/4, 3, 6);
         
@@ -52,10 +61,9 @@ public class Breakout extends BasicGame
     @Override
     public void update (GameContainer gc, int delta) throws SlickException
     {
+        // Active game
         if (gameActiveCounter <= 0.0f)
-        {
-            whiteFadeAlpha = 0.0f;
-            
+        {   
             // Input events
             Input input = gc.getInput();
     
@@ -69,22 +77,75 @@ public class Breakout extends BasicGame
                 racket.goLeft(Racket.hspeed * delta);
             }
             
-            if (input.isKeyDown(Input.KEY_SPACE))
+            if (input.isKeyDown(Input.KEY_SPACE) && racket.haveAttachedBall())
             {
-                invokeDefeat();
+                ballSpeed[0] = 0.0f;
+                ballSpeed[1] = -0.8f;
+                racket.detachBall();
             }
-        }
-        else
-        {
-            // Decrease game active counter
-            gameActiveCounter -= 0.0018f * delta;
-            if (gameActiveCounter <= 0.0f) gameActiveCounter = 0.0f;
             
+            // Speed ball
+            ball.setX(ball.getX()+ballSpeed[0]*(float)delta*0.5f);
+            ball.setY(ball.getY()+ballSpeed[1]*(float)delta*0.5f);
+            //ballSpeed[0] *= 1.0001f*(float)delta*0.1f;
+            //ballSpeed[1] *= 1.0001f*(float)delta*0.1f;
+            
+            // Collision with bricks
+            for (Brick b : bricks)
+            {
+                if (Collision.rectangle(
+                    ball.getX(), ball.getY(), Ball.w, Ball.h,
+                    b.getX(), b.getY(), b.getWidth(), b.getHeight()))
+                {
+                    bricks.remove(b);
+                    ballSpeed[1] = -ballSpeed[1];
+                    break;
+                }
+            }
+            
+            // Collision with racket
+            if (Collision.rectangle(
+                    ball.getX(), ball.getY(), Ball.w, Ball.h,
+                    racket.getX(), racket.getY(), Racket.w, Racket.h))
+            {
+                if (racket.getY()-ball.getY()-Ball.h+4 < 0)
+                    invokeDefeat();
+                else
+                    ballSpeed[1] = -ballSpeed[1];
+                
+            }
+            
+            // Collision with bottom
+            if (Collision.rectangle(
+                    ball.getX(), ball.getY(), Ball.w, Ball.h,
+                    0, gc.getHeight()-10, gc.getWidth(), 10))
+                invokeDefeat();
+            
+            // Collision with top
+            if (Collision.rectangle(
+                    ball.getX(), ball.getY(), Ball.w, Ball.h,
+                    0, 0, gc.getWidth(), 0))
+                ballSpeed[1] = -ballSpeed[1];
+        }
+        // Inactive game
+        else
+        {   
             // Decrease white fade alpha
             whiteFadeAlpha -= 0.0025f * delta;
             if (whiteFadeAlpha <= 0.0f) whiteFadeAlpha = 0.0f;
             
             racket.goUp(Racket.vspeed * delta);
+            
+            // Decrease game active counter
+            gameActiveCounter -= 0.0018f * delta;
+            if (gameActiveCounter <= 0.0f)
+            {
+                gameActiveCounter = 0.0f;
+                whiteFadeAlpha = 0.0f;
+                racket.goActivePosition();
+                ballSpeed[0] = 0.0f;
+                ballSpeed[1] = 0.0f;
+            }
         }
     }
 
@@ -96,8 +157,9 @@ public class Breakout extends BasicGame
             for (int y = 0; y < gc.getHeight(); y += bgImage.getHeight())
                 bgImage.draw(x, y);
         
-        // Render racket
+        // Render racket and ball
         racket.draw();
+        ball.draw();
         
         // Render bricks
         for (Brick b : bricks) b.draw();
@@ -115,13 +177,14 @@ public class Breakout extends BasicGame
         gameActiveCounter = 1.0f;
         whiteFadeAlpha = 1.0f;
         racket.resetPosition();
+        racket.attachBall(ball);
     }
 
     public static void main (String[] args) throws SlickException
     {
         AppGameContainer app = new AppGameContainer(new Breakout());
         app.setDisplayMode(800, 600, false);
-        app.setTargetFrameRate(200);
+        app.setTargetFrameRate(2000);
         app.start();
     }
 }
