a6d5710657bf080fad02651c4523a80a5af37e74
thomas.dimeco@gmail.com <thomas.dimeco@gmail.com@13a1db8c-4117-8554-9844-c95902a293b6>
2012-04-09 13:02:35 +0000

    Core: Added Transition superclass

--- trunk/src/nebula/core/intro/IntroJeu.java
+++ trunk/src/nebula/core/intro/IntroJeu.java
@@ -1,80 +1,30 @@
 package nebula.core.intro;
 
-import nebula.core.NebulaGame;
-
-import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class IntroJeu extends BasicGameState
-{
 
-	Image image, cadre;
-	StringBuilder s;
-	float scale;
-	float cpt = 0f;
-	float x = -1000,y = -1000;
+public class IntroJeu extends Transition
+{
+	/* Transition ID */
+	@Override public int getID () { return 3; }
 	
 	@Override
-	public void init(GameContainer container, StateBasedGame game) throws SlickException 
+	public void init (GameContainer gc, StateBasedGame game)
+	    throws SlickException
+	    
 	{
-		image = new Image("ressources/images/histoire/nebula_intro.jpg");
-		cadre = new Image("ressources/images/miscellaneous/cadre.png");
-		scale = 1.0f;
-		((NebulaGame) game).getUFont().loadGlyphs();
-		s = new StringBuilder();
-		s.append("Bidibop est un alien. Et aujourd'hui il a 10 ans.\n");
-		s.append("C'est a dire qu'aujourd'hui il peut conduire son\n");
-		s.append("propre vaisseau spatial !\n");
-		s.append("Et ca tombe bien car Bidibop construit depuis\n");
-		s.append("2 ans une superbe soucoupe rouge avec son papa.");
-		
+	    // Call super method
+	    super.init(gc, game);
+	    
+	    this.setTransitionImage("ressources/images/histoire/nebula_intro.jpg");
+	    this.setTransitionText(
+	        "Bidibop est un alien. Et aujourd'hui il a 10 ans.\n" +
+	        "C'est a dire qu'aujourd'hui il peut conduire son\n" +
+	        "propre vaisseau spatial !\n" +
+	        "Et ca tombe bien car Bidibop construit depuis\n" +
+	        "2 ans une superbe soucoupe rouge avec son papa."
+	    );
 	}
-
-	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException 
-	{
-		image.draw(0, 0, scale * container.getWidth(), scale * container.getHeight());
-		cadre.draw(x, y, container.getWidth()*0.90f, container.getHeight() * 0.3f);
-		((NebulaGame)game).getUFont().drawString(x + container.getWidth()*0.05f, y + container.getHeight()*0.03f, s.toString());
-	}
-
-	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException 
-	{
-		Input input = container.getInput();
-		
-		if(input.isKeyDown(Input.KEY_ENTER))
-		{
-			((NebulaGame)game).next(this.getID(),1);
-		}
-		
-		if(input.isKeyDown(Input.KEY_ESCAPE))
-		{
-			((NebulaGame)game).enterState(0);
-		}
-		
-		if(cpt < 3)
-		{
-			cpt += delta * 0.001f;
-		}
-		else
-		{
-			x = container.getWidth() * 0.05f;
-			y = container.getHeight() * 0.7f;
-		}
-		
-	}
-
-	@Override
-	public int getID() 
-	{
-		return 3;
-	}
-
 }
---
+++ trunk/src/nebula/core/intro/Transition.java
@@ -0,0 +1,209 @@
+package nebula.core.intro;
+
+import nebula.core.NebulaGame;
+import nebula.core.NebulaGame.TransitionType;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Custom base transition class
+ */
+public abstract class Transition extends BasicGameState
+{
+    protected NebulaGame nebulaGame;
+    private String text;
+    private float time = Float.NEGATIVE_INFINITY;
+    private float timeBeforeText = 3000.0f;
+    private TransitionType transitionType = TransitionType.Default;
+    private Image image, face, frame;
+    private Rectangle frameRect = new Rectangle(0.0f, 0.0f, 0.0f, 0.0f);
+    private float screenw, screenh;
+    
+    private static final float FRAME_MARGIN = 24.0f;
+    private static final float FRAME_HEIGHT = 300.0f;
+    private static final float FACE_WIDTH = 250.0f;
+    
+    
+    @Override
+    public void init (GameContainer gc, StateBasedGame game)
+        throws SlickException
+    {
+        // Backup game
+        this.nebulaGame = (NebulaGame)game;
+        
+        // Screen size
+        screenw = gc.getScreenWidth();
+        screenh = gc.getScreenHeight();
+    }
+
+    @Override
+    public final void update (GameContainer gc, StateBasedGame game, int delta)
+        throws SlickException
+    {
+        Input input = gc.getInput();
+        
+        // Next state key
+        if(input.isKeyDown(Input.KEY_ENTER))
+            nebulaGame.next(this.getID(), TransitionType.HorizontalSplit);
+        
+        // Escape key
+        if(input.isKeyDown(Input.KEY_ESCAPE))
+            nebulaGame.enterState(0);
+        
+        // Text time counter
+        if (timeBeforeText > 0) timeBeforeText -= delta;
+        else timeBeforeText = 0.0f;
+        
+        // Transition time counter if needed
+        if (time != Float.NEGATIVE_INFINITY)
+        {
+            if (time > 0) time -= delta;
+            else gotoNextState();
+        }
+    }
+    
+    @Override
+    public final void render (GameContainer gc, StateBasedGame game, Graphics g)
+        throws SlickException
+    {
+        // Render image
+        if (image != null)
+            image.draw(0, 0, gc.getWidth(), gc.getHeight());
+        
+        // Render text and frame
+        if (text != null && frame != null && timeBeforeText == 0.0f)
+        {
+            frame.draw(
+                frameRect.getX(), frameRect.getY(),
+                frameRect.getWidth(), frameRect.getHeight());
+            
+            nebulaGame.getUFont().drawString(
+                frameRect.getX() + 64.0f, frameRect.getY() + 32.0f,
+                text.toString());
+            
+            if (face != null)
+                face.draw(
+                    FRAME_MARGIN,
+                    screenh - FRAME_MARGIN - FRAME_HEIGHT,
+                    FACE_WIDTH,
+                    FRAME_HEIGHT
+                );
+        }
+    }
+    
+    /**
+     * Goto next state
+     */
+    protected void gotoNextState ()
+    {
+        nebulaGame.next(this.getID(), transitionType);
+    }
+    
+    /**
+     * Create frame rectangle
+     */
+    private void createFrameRect ()
+    {
+        if (face == null)
+        {
+            frameRect.setBounds(
+                FRAME_MARGIN,
+                screenh - FRAME_MARGIN - FRAME_HEIGHT,
+                screenw - 2*FRAME_MARGIN,
+                FRAME_HEIGHT
+            );
+        }
+        else
+        {
+            frameRect.setBounds(
+                FACE_WIDTH + 2*FRAME_MARGIN,
+                screenh - FRAME_MARGIN - FRAME_HEIGHT,
+                screenw - 3*FRAME_MARGIN - FACE_WIDTH,
+                FRAME_HEIGHT
+            );
+        }
+    }
+    
+    /**
+     * Set the transition duration
+     * @param time Time in milliseconds
+     */
+    protected void setTransitionTime (float time)
+    {
+        this.time = time;
+    }
+    
+    /**
+     * Set the transition time before text
+     * @param time Time in milliseconds
+     */
+    protected void setTransitionTimeBeforeText (float time)
+    {
+        this.timeBeforeText = time;
+    }
+    
+    /**
+     * Set the transition text
+     * @param text Transition text
+     */
+    protected void setTransitionText (String text)
+    {
+        this.text = text;
+        
+        try {
+            frame = new Image("ressources/images/miscellaneous/cadre.png");
+            nebulaGame.getUFont().loadGlyphs();
+        }
+        catch (SlickException exc) {
+            exc.printStackTrace();
+        }
+        
+        createFrameRect();
+    }
+    
+    /**
+     * Set the transition image
+     * @param path The image path
+     */
+    protected void setTransitionImage (String path)
+    {
+        try {
+            image = new Image(path);
+        }
+        catch (SlickException exc) {
+            exc.printStackTrace();
+        }
+    }
+    
+    /**
+     * Set the transition face character
+     * @param path The image path
+     */
+    protected void setTransitionFace (String path)
+    {
+        try {
+            face = new Image(path);
+        }
+        catch (SlickException exc) {
+            exc.printStackTrace();
+        }
+        
+        createFrameRect();
+    }
+    
+    /**
+     * Set transition end type
+     * @param transitionType The transition type
+     */
+    protected void setTransitionType (TransitionType transitionType)
+    {
+        this.transitionType = transitionType;
+    }
+}
