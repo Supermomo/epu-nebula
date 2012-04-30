898b483a288950c9518b24a830df0d30ed1e8c5f
thomas.dimeco <thomas.dimeco@gmail.com>
2012-04-30 13:19:56 +0000

    Core: Added default background for all states

---
+++ src/nebula/core/state/EndMenuState.java
@@ -0,0 +1,37 @@
+package nebula.core.state;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import nebula.core.NebulaGame.NebulaState;
+
+
+/**
+ * End menu state
+ */
+public class EndMenuState extends AbstractMenuState
+{
+    @Override
+    public void init (GameContainer gc, StateBasedGame game)
+        throws SlickException
+    {
+        // Call super method
+        super.init(gc, game);
+
+        // Add menu items
+        setMenuTitle("Fin");
+        addMenuItem("Bravo, vous avez fini l'aventure !", false);
+        addMenuSpaces(1);
+        addMenuItem("Quitter", true);
+    }
+    
+    @Override
+    protected void indexSelectedEvent (int index, StateBasedGame game)
+    {
+        nebulaGame.enterState(NebulaState.MainMenu.id);
+    }
+
+    @Override
+    public int getID () { return NebulaState.EndMenu.id; }
+}
