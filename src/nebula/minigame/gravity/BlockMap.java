68e7880981a49f86de81faf32238c7d7646984c0
dionysos.kluh <dionysos.kluh@gmail.com>
2012-05-15 17:16:57 +0000

    Gravity : 	- Ajout de la gestion des maps 	- Augmentés : 		Vitesse déplacement joueur 		Gravité 		Vitesse déplacement scroll map sur l'axe Y 	- Ajout et modifications des 3 maps de base 	- Modification fichiers son

--- src/nebula/minigame/gravity/BlockMap.java
+++ src/nebula/minigame/gravity/BlockMap.java
@@ -5,21 +5,21 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class BlockMap {
-	
-	
-	
+
+
+
 	private TiledMap map;
 	private BlockType[][] blocks;
 	private int tileHeight;
 	private int tileWidth;
-	
+
 	private Point depart;
-	
+
 	public Point getDepart() {
 		return depart;
 	}
 	public void setDepart(float x, float y) {
-		depart = new Point(Math.round(x/tileWidth)*tileWidth,Math.round(y/tileHeight)*tileHeight);
+		depart = new Point((int) Math.floor(x/tileWidth)*tileWidth,(int) Math.floor(y/tileHeight)*tileHeight);
 	}
 
 	public BlockMap(String reference) throws SlickException {
@@ -29,39 +29,44 @@ public class BlockMap {
 		// Récupération des données sur la carte
 		tileHeight = map.getTileHeight();
 		tileWidth = map.getTileWidth();
-		
+
 		// Construction de la matrice de blocks
 		blocks = new BlockType[map.getWidth()][map.getHeight()];
-		
+
 		// --- Récupération des tileSets de blocs
 		for(int xAxis=0;xAxis<map.getWidth(); xAxis++) {
 			for (int yAxis=0;yAxis<map.getHeight(); yAxis++) {
 				//--- Éléments bloquant
-				if (map.getTileId(xAxis, yAxis, 0)!=0) blocks[xAxis][yAxis] = BlockType.BLOCK;
-				else if(map.getTileId(xAxis, yAxis, 1)!=0) blocks[xAxis][yAxis] = BlockType.END;
-				else if(map.getTileId(xAxis, yAxis, 3)!=0) blocks[xAxis][yAxis] = BlockType.DEATH;
+				if (map.getTileId(xAxis, yAxis, map.getLayerIndex("block"))!=0) blocks[xAxis][yAxis] = BlockType.BLOCK;
+				else if(map.getTileId(xAxis, yAxis, map.getLayerIndex("end"))!=0) blocks[xAxis][yAxis] = BlockType.END;
+				else if(map.getTileId(xAxis, yAxis, map.getLayerIndex("death"))!=0) blocks[xAxis][yAxis] = BlockType.DEATH;
 			}
 		}
-		
-		// Récupération des Objets Spawn
-		for(int i=0; i<map.getObjectGroupCount(); i++) {
-			for(int j=0; j<map.getObjectCount(i); j++) {
-				if("start".equals(map.getObjectName(i,j)) && "start2".equals(map.getObjectType(i,j)))
-					setDepart(map.getObjectX(i, j), map.getObjectY(i, j));
+
+		// Récupération des Objets
+		int id_layer_objet = 0; // Impossible de récupérer les layers d'objets avec le parser TiledMap de Slick
+		for(int j=0; j<map.getObjectCount(id_layer_objet); j++) {
+			// Récupération du départ
+			if("start".equals(map.getObjectName(id_layer_objet,j)) && "default".equals(map.getObjectType(id_layer_objet,j))) {
+				setDepart(map.getObjectX(id_layer_objet, j)/60*60+60, map.getObjectY(id_layer_objet, j)/60*60+60);
+			} else if("warp".equals(map.getObjectName(id_layer_objet,j))) {
+				map.getObjectX(0, j);
+				map.getObjectY(0, j);
 			}
 		}
+
 		if(depart==null) throw new SlickException("Aucun objet start enregistré dans la carte "+reference);
-		
+
 	}
 
 	public TiledMap getTiledMap() {
 		return map;
 	}
-	
+
 	public BlockType[][] getBlocks() {
 		return blocks;
 	}
-	
+
 	/**
 	 * Regarde si au moins un des deux points est placé sur unr tuile block
 	 * @param p Les deux points à tester
@@ -69,11 +74,11 @@ public class BlockMap {
 	 */
 	public boolean deplacementBloquant(Point[] p) {
 		if( blocks[(int) (p[0].getX()/tileWidth)][(int) (p[0].getY()/tileHeight)] == BlockType.BLOCK
-		||	blocks[(int) (p[1].getX()/tileWidth)][(int) (p[1].getY()/tileHeight)] == BlockType.BLOCK)
+				||	blocks[(int) (p[1].getX()/tileWidth)][(int) (p[1].getY()/tileHeight)] == BlockType.BLOCK)
 			return true;
 		return false;
 	}
-	
+
 	/**
 	 * Regarde si au moins un des deux points est placé sur unr tuile block
 	 * @param p Les deux points à tester
