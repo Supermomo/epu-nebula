356d2a5c63ed001022d701fba5e738a6102d0b19
maugard.matthieu@gmail.com <maugard.matthieu@gmail.com@13a1db8c-4117-8554-9844-c95902a293b6>
2012-05-21 12:07:48 +0000

    cool

--- trunk/src/nebula/minigame/boss/Tourelle.java
+++ trunk/src/nebula/minigame/boss/Tourelle.java
@@ -1,6 +1,62 @@
 package nebula.minigame.boss;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Tourelle 
 {
+	private static Image image = null;
+	private float x;
+	private float y;
+	
+	public Tourelle() throws SlickException
+	{
+		image = new Image("ressources/images/boss/ship.png");
+		this.x = x; 
+		this.y = y;
+	}
+	
+	public void rotate(float delta, boolean droite)
+	{	 
+		if(!droite)
+		{
+        	this.getImage().rotate(-0.2f * delta);
+		}
+        else
+        {
+        	this.getImage().rotate(0.2f * delta);
+        }
+	}
 
+	public static Image getImage() 
+	{
+		return image;
+	}
+
+	public static void setImage(Image image) 
+	{
+		Tourelle.image = image;
+	}
+
+	public float getX() 
+	{
+		return x;
+	}
+
+	public void setX(float x) 
+	{
+		this.x = x;
+	}
+
+	public float getY() 
+	{
+		return y;
+	}
+
+	public void setY(float y) 
+	{
+		this.y = y;
+	}
+	
+	
 }
