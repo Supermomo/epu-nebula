c7dab8126e3e7aac5d8f111031b61a16eba35ca8
gwenn.aubert <gwenn.aubert@gmail.com>
2012-05-23 16:58:50 +0000

    Delete des prints sauvages Ajout d'une fonction pour attirer les lucioles avec Ctrl Delete d'une image inutilisé Delete du TODO

--- src/nebula/minigame/spaceShepherd/Flock.java
+++ src/nebula/minigame/spaceShepherd/Flock.java
@@ -18,8 +18,6 @@ public class Flock extends SteeringEntity {
 
 	private ArrayList<Line> remaining = null;;
 
-	private int margin = 40;
-
 	public Flock(int x, int y, float maxSpeed, int fieldx, int fieldy,
 			int flNb, float attCoef) {
 		super(x, y, maxSpeed, fieldx, fieldy);
@@ -49,16 +47,10 @@ public class Flock extends SteeringEntity {
 			int cpt = 0;
 			while (!isValidTrajectory(newPos, fences) && cpt < 300) {
 				newPos = getFlockingMouvment(fl, delta, fences);
-				// System.out.println("flock pb");
-				System.out.println("flock failure");
-				// fl.setMaxSpeed((float) (fl.getMaxSpeed()*0.9));
-				// fl.setMaxRotation((float) (fl.getMaxRotation()*1.5));
-				// newPos=fl.moveRandomly(delta, fences);
 				cpt++;
 			}
 
 			if (cpt == 300) {
-				System.out.println("30000000000000000000000000");
 				newPos = super.getPosition();
 			}
 			fl.setPosition(newPos);
@@ -67,6 +59,7 @@ public class Flock extends SteeringEntity {
 
 	/**
 	 * return true if all the flocker are in the vortex
+	 * 
 	 * @param target
 	 * @param radius
 	 * @return
@@ -139,27 +132,7 @@ public class Flock extends SteeringEntity {
 	 */
 	public boolean isEnded(final ArrayList<Line> fences, Vector2f target) {
 
-		Line left = new Line(margin, margin, margin, super.getYfield() - margin);
-		Line top = new Line(margin, margin, super.getXfield() - margin, margin);
-		Line bottom = new Line(margin, super.getYfield() - margin, super
-				.getXfield()
-				- margin, super.getYfield() - margin);
-		Line right = new Line(super.getXfield() - margin, margin, super
-				.getXfield()
-				- margin, super.getYfield() - margin);
-
-		ArrayList<Line> bords = new ArrayList<Line>();
-		bords.add(left);
-		bords.add(top);
-		bords.add(bottom);
-		bords.add(right);
-
-		/*
-		 * ArrayList<Line> remaining=new ArrayList<Line>(); for(Line le:fences){
-		 * remaining.add(le); }
-		 */
-
-		//remaining = fences;
+		// remaining = fences;
 
 		if (remaining == null) {
 			remaining = new ArrayList<Line>();
@@ -184,102 +157,56 @@ public class Flock extends SteeringEntity {
 			shape = inclusiveSubList(remaining, cpt);
 
 			Vector2f inter = intersectItself(shape);
-			Vector2f interBord[] = intersectBord(shape, bords);
 
-			// Si on peut faire une forme (avec les lignes ou les bords)
-			if (inter != null || interBord != null) {
+			// Création de la forme
+			if (inter != null) {
 
 				path = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
 
-				// Création de la forme
-				if (inter != null) {
-					// Recupére les ligne coupé par la ligne courante
-					ArrayList<Line> interList = intersectLine(shape, l);
-					// Parcours des lignes coupants la ligne courante
-					for (int e = 0; e < interList.size(); e++) {
+				// Recupére les ligne coupé par la ligne courante
+				ArrayList<Line> interList = intersectLine(shape, l);
+				// Parcours des lignes coupants la ligne courante
+				for (int e = 0; e < interList.size(); e++) {
 
-						path = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
-						Line lili = interList.get(e);
-						inter = lili.intersect(l, true);
-						// On remet la liste de travail a son état au début de
-						// la boucle
-						shape = inclusiveSubList(remaining, cpt);
+					path = new GeneralPath(GeneralPath.WIND_EVEN_ODD);
+					Line lili = interList.get(e);
+					inter = lili.intersect(l, true);
+					// On remet la liste de travail a son état au début de
+					// la boucle
+					shape = inclusiveSubList(remaining, cpt);
 
-						// Retire toutes les lignes précédant la ligne coupé par
-						// la ligne courante
-						while (!shape.get(0).equals(lili)) {
-							shape.remove(0);
-						}
-
-						// On crée la forme
-						path.moveTo(inter.x, inter.y);
-						int h = 0;
-						while (h < shape.size() - 1
-								&& !(e + 1 < interList.size() && shape.get(h)
-										.equals(interList.get(e + 1)))) {
-
-							Line li = shape.get(h);
-							path.lineTo(li.getX2(), li.getY2());
-							h++;
-						}
-						if (h != shape.size() - 1) {
-							Vector2f point = interList.get(e + 1).intersect(l,
-									true);
-							path.lineTo(point.x, point.y);
-						}
-						path.lineTo(inter.x, inter.y);
-
-						// Si le leader du flock est dans une forme et que la
-						// cible n'y est pas
-						if (path.contains(target.x, target.y) != path.contains(
-								super.getPosition().x, super.getPosition().y)) {
-							System.out.println("e : " + e + " l :" + cpt);
-							return true;
-						}
-
+					// Retire toutes les lignes précédant la ligne coupé par
+					// la ligne courante
+					while (!shape.get(0).equals(lili)) {
+						shape.remove(0);
 					}
 
-				} else if (interBord != null) {
+					// On crée la forme
+					path.moveTo(inter.x, inter.y);
+					int h = 0;
+					while (h < shape.size() - 1
+							&& !(e + 1 < interList.size() && shape.get(h)
+									.equals(interList.get(e + 1)))) {
 
-					System.out.println("border intersection");
-
-					// Point in the middle of the line formed by the two bord
-					// intersection
-					Vector2f middle = new Vector2f(
-							(interBord[0].x + interBord[1].x) / 2,
-							(interBord[0].y + interBord[1].y) / 2);
-
-					System.out.println(" middle : x " + middle.x + " y : "
-							+ middle.y);
-					// Closest corner to that point
-					Vector2f closesstCorner = getClosestCorner(middle);
-
-					System.out.println("closest : x : " + closesstCorner.x
-							+ " y : " + closesstCorner.y);
-					// Recupére la premiere ligne coupant un bord
-					Line border = new Line(closesstCorner, interBord[0]);
-					// border=intersectLine(shape, l);
-					int indexFirstLine = shape.indexOf(border) + 1;
-
-					System.out.println("indice : "
-							+ remaining.indexOf(shape.get(indexFirstLine)));
-					// Ajout du coin de départ et de la premiere ligne (du coin
-					// a la premiere intersection avec le bord)
-					path.moveTo(closesstCorner.x, closesstCorner.y);
-					path.lineTo(shape.get(indexFirstLine).getX1(), shape.get(
-							indexFirstLine).getY1());
-
-					for (int h = indexFirstLine; h < shape.size(); h++) {
 						Line li = shape.get(h);
 						path.lineTo(li.getX2(), li.getY2());
+						h++;
 					}
-					path.lineTo(closesstCorner.x, closesstCorner.y);
+					
+					if (h != shape.size() - 1) {
+						Vector2f point = interList.get(e + 1)
+								.intersect(l, true);
+						path.lineTo(point.x, point.y);
+					}
+					path.lineTo(inter.x, inter.y);
 
-					// On retire les ligne de la forme crée de la list pour le
-					// pas les traiter 2 fois.
-					// substractList(remaining, shape);
+					// Si le leader du flock est dans une forme et que la
+					// cible n'y est pas
+					if (path.contains(target.x, target.y) != path.contains(
+							super.getPosition().x, super.getPosition().y)) {
+						return true;
+					}
 
-					cpt++;
 				}
 
 				// Si le leader du flock est dans une forme et que la cible n'y
@@ -297,41 +224,6 @@ public class Flock extends SteeringEntity {
 	}
 
 	/**
-	 * return the corner which is the closest to this point
-	 * 
-	 * @param point
-	 * @return
-	 */
-	private Vector2f getClosestCorner(Vector2f point) {
-		Vector2f tab[] = new Vector2f[4];
-		tab[0] = new Vector2f(margin, margin);
-		tab[1] = new Vector2f(margin, super.getYfield() - margin);
-		tab[2] = new Vector2f(super.getXfield() - margin, margin);
-		tab[3] = new Vector2f(super.getXfield() - margin, super.getYfield()
-				- margin);
-
-		Vector2f closest = tab[0];
-		for (Vector2f ve : tab) {
-			if (point.distance(ve) < point.distance(closest)) {
-				closest = ve;
-			}
-		}
-		return closest;
-	}
-
-	/**
-	 * remove the elements of the sub list from the first list
-	 * 
-	 * @param list
-	 * @param sub
-	 */
-	private void substractList(ArrayList<Line> list, ArrayList<Line> sub) {
-		for (Line l : sub) {
-			list.remove(l);
-		}
-	}
-
-	/**
 	 * Returnthe first intersection of this shape with itself
 	 * 
 	 * @param shape
@@ -344,32 +236,6 @@ public class Flock extends SteeringEntity {
 					if (Math.abs(shape.indexOf(l1) - shape.indexOf(l2)) > 1) {
 						return l1.intersect(l2, true);
 					}
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Return the next Two intersection of the shape with some borders or null
-	 * 
-	 * @param shape
-	 * @param Bords
-	 * @return
-	 */
-	private Vector2f[] intersectBord(ArrayList<Line> shape,
-			ArrayList<Line> Bords) {
-
-		Vector2f tab[] = new Vector2f[2];
-		int cpt = 0;
-		for (Line b : Bords) {
-			for (Line l : shape) {
-				tab[cpt] = l.intersect(b, true);
-				if (tab[cpt] != null) {
-					cpt++;
-				}
-				if (cpt == 2) {
-					return tab;
 				}
 			}
 		}
