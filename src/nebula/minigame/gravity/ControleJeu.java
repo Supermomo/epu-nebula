6077864efdb209a3afac043fe7924a5db5e692c0
dionysos.kluh <dionysos.kluh@gmail.com>
2012-05-29 19:15:51 +0000

    Gravity : Ajout de 3 maps par niveaux (total : 8 par niveaux)

--- src/nebula/minigame/gravity/ControleJeu.java
+++ src/nebula/minigame/gravity/ControleJeu.java
@@ -19,8 +19,6 @@ public class ControleJeu {
 	}
 
 	public void inputJoueur(Input input, int delta) {
-		//TODO Ajouter le contrôle du délais entre deux changements de gravité
-
 		// -- Déplacement Hero
 		// Déplacement à gauche
 		if (input.isKeyDown(Input.KEY_LEFT)) {
@@ -30,30 +28,11 @@ public class ControleJeu {
 		else if (input.isKeyDown(Input.KEY_RIGHT)) {
 			modeleJeu.deplacementDroite(delta);
 		}
-		/*else if (input.isKeyDown(Input.KEY_UP)) {
-			
-		}
-		else if (input.isKeyDown(Input.KEY_DOWN)) {
-			System.out.println(modeleJeu.getHero().getPosition());
-		}
 
-		// Courrir
-		if(input.isKeyPressed(Input.KEY_LCONTROL)) {
-			//TODO Passer en KeyDown
-			//modeleJeu.courrir();
-		}
-		*/
 		// Changer la gravité
 		if(input.isKeyPressed(Input.KEY_SPACE))
 			modeleJeu.inverserGravite();
-		else {
-			//modeleJeu.deplacementBas(modeleJeu.getGravite()*delta);
-			/*		
-			 * // Le personnage s'arréte
-			 * if(!hero.isStill() && hero.getStill()<35) hero.incStill();
-			 * else if(hero.getStill()!=-1) hero.setAnimStill();
-			 */
-		}
+		
 		modeleJeu.appliquerGravite(delta);
 		// -- Fin - déplacement Hero
 
